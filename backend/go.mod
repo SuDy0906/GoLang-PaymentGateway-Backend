82a42b6809a8ea931c4354c620ca7038cd599615
SuDy0906 <127987188+SuDy0906@users.noreply.github.com>
2024-07-10 16:07:55 +0530

    Newest

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/SuDy0906/PaymentGateway/backend
+
+go 1.22.5
+
+require github.com/stripe/stripe-go/v74 v74.30.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+
+	"github.com/stripe/stripe-go/v74"
+	"github.com/stripe/stripe-go/v74/paymentintent"
 )
 
 func main() {
-
+	stripe.Key = "sk_test_51Pak73ENKr6eKzxfK6tQN7inq5ZLICmStibyO3KHTxnmCA6Du7mGUaQf2rqGGZakfDicKnwQnL8FqqtStwYHYopw00Rk5pu0nY"
 	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
 	http.HandleFunc("/health", handleHealth)
 	fmt.Println("Listening on localhost:4242")
-	var err error = http.ListenAndServe("localhost:4242", nil)
-	if err != nil {
+	if err := http.ListenAndServe("localhost:4242", nil); err != nil {
 		log.Fatal(err)
 	}
-
 }
 
 func handleCreatePaymentIntent(writer http.ResponseWriter, request *http.Request) {
@@ -42,13 +45,56 @@ func handleCreatePaymentIntent(writer http.ResponseWriter, request *http.Request
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Println(req.ProductId)
+
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(calculateOrderAmount(req.ProductId)),
+		Currency: stripe.String(string(stripe.CurrencyUSD)),
+		AutomaticPaymentMethods: &stripe.PaymentIntentAutomaticPaymentMethodsParams{
+			Enabled: stripe.Bool(true),
+		},
+	}
+
+	paymentIntent, err := paymentintent.New(params)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var response struct {
+		ClientSecret string `json:"clientSecret"`
+	}
+	response.ClientSecret = paymentIntent.ClientSecret
+
+	var buf bytes.Buffer
+	err = json.NewEncoder(&buf).Encode(response)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	_, err = io.Copy(writer, &buf)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
+
 func handleHealth(writer http.ResponseWriter, request *http.Request) {
 	response := []byte("Server is up and running!")
-
 	_, err := writer.Write(response)
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func calculateOrderAmount(productId string) int64 {
+	switch productId {
+	case "Forever Pants":
+		return 26000
+	case "Forever Shirt":
+		return 15500
+	case "Forever Shorts":
+		return 30000
+	}
+	return 0
 }
