a99d9e975b598161fde9fba611efd4ef70855b25
SuDy0906 <127987188+SuDy0906@users.noreply.github.com>
2024-07-10 14:55:57 +0530

    first commit

---
+++ backend/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
+	http.HandleFunc("/health", handleHealth)
+	fmt.Println("Listening on localhost:4242")
+	var err error = http.ListenAndServe("localhost:4242", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func handleCreatePaymentIntent(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "POST" {
+		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req struct {
+		ProductId string `json:"product_id"`
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+		Address1  string `json:"address_1"`
+		Address2  string `json:"address_2"`
+		City      string `json:"city"`
+		State     string `json:"state"`
+		Zip       string `json:"zip"`
+		Country   string `json:"country"`
+	}
+
+	err := json.NewDecoder(request.Body).Decode(&req)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(req.ProductId)
+}
+func handleHealth(writer http.ResponseWriter, request *http.Request) {
+	response := []byte("Server is up and running!")
+
+	_, err := writer.Write(response)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
